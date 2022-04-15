9378796651598b022f9f232ddec31d88697132b9
Kyrylo Mieshkov <mieshkov.k@gmail.com>
2022-04-15 12:26:54 -0400

    second method added, data works both with strings and integers

--- src/test/java/DataDriven.java
+++ src/test/java/DataDriven.java
@@ -1,5 +1,7 @@
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -9,31 +11,21 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class dataDriven {
+public class DataDriven {
 
-	public static String sheetName = "TestCases";
+	private static final String absolutePath = "/Users/kmieshkov/Projects/IdeaProjects/maven-excel";
 
-	public static void main(String[] args) throws IOException {
-
-		parseTestcases("/testcases.xlsx");
-		parseTestData("/testdata.xlsx");
-
-	}
-
-	private static void parseTestData(String fileName) throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("/Users/kmieshkov/Projects/IdeaProjects/maven-excel" + fileName);
-		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-		
-	}
-
-	public static void parseTestcases(String fileName) throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("/Users/kmieshkov/Projects/IdeaProjects/maven-excel" + fileName);
+	public HashMap<String, String> getFullData(String fileName, String sheetName, String testCaseId) throws IOException {
+		FileInputStream fileInputStream = new FileInputStream(absolutePath + fileName);
 		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
 
 		ArrayList<String> columnNames = new ArrayList<>();
-		HashMap<String, ArrayList<String>> data = new HashMap<>();
-		
+		HashMap<String, String> data = new HashMap<>();
+
 		XSSFSheet sheet = workbook.getSheet(sheetName);
+		if (sheet == null) {
+			sheet = workbook.getSheetAt(workbook.getActiveSheetIndex());
+		}
 		Iterator<Row> rows = sheet.iterator();
 
 		// iterator for each row in a sheet
@@ -42,24 +34,73 @@ public class dataDriven {
 			Iterator<Cell> cellIterator = row.cellIterator();
 			ArrayList<String> array = new ArrayList<>();
 
-			// iterator for each cell in a row
-			while(cellIterator.hasNext()) {
+			// iterator for each cell in a row\
+			int needleRow = 0;
+			while (cellIterator.hasNext()) {
 				Cell cellValue = cellIterator.next();
 				if (j == 0) {
-					columnNames.add(cellValue.getStringCellValue());
-					data.put(cellValue.getStringCellValue(), new ArrayList<>());
-				} else {
-					array.add(cellValue.getStringCellValue());
+					columnNames.add(formattedCellValue(cellValue));
+				} else if (formattedCellValue(cellValue).equals(testCaseId) || needleRow == 1) {
+					needleRow = 1;
+					array.add(formattedCellValue(cellValue));
 				}
 			}
 
-			// save data in format "ID" => ["TC-01", "TC-02" ...]
-			// save data in format "Title" => ["....", "...." ...]
+			// save data in format "["ID": "TC-01", "Test Data": "Data123", ...]
 			for (int k = 0; k < array.size(); k++) {
-				data.get(columnNames.get(k)).add(array.get(k % columnNames.size()));
+				System.out.printf("%s = %s\n", columnNames.get(k), array.get(k));
+				data.put(columnNames.get(k), array.get(k));
 			}
 		}
-
-		System.out.println(data);
+		return(data);
 	}
+
+	public ArrayList<String> getData(String fileName, String sheetName, String rowName) throws IOException {
+		ArrayList<String> a = new ArrayList<String>();
+		FileInputStream fileInputStream = new FileInputStream(absolutePath + fileName);
+		XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+		ArrayList<String> array = new ArrayList<>();
+
+		int sheets = workbook.getNumberOfSheets();
+		for (int i = 0; i < sheets; i++) {
+
+			if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) {
+				XSSFSheet sheet = workbook.getSheetAt(i);
+				//Identify Testcases column by scanning the entire 1st row
+				Iterator<Row> rows = sheet.iterator();// sheet is collection of rows
+				Row firstRow = rows.next();
+				Iterator<Cell> cellIterator = firstRow.cellIterator();//row is collection of cells
+
+				// Find the column with names
+				int column = 0;
+				for (int k = 0; cellIterator.hasNext(); k++) {
+					Cell value = cellIterator.next();
+					if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+						column = k;
+					}
+				}
+				while (rows.hasNext()) {
+					Row row = rows.next();
+
+					// grab all cells for Purchase
+					if (row.getCell(column).getStringCellValue().equalsIgnoreCase(rowName)) {
+						Iterator<Cell> cell = row.cellIterator();
+						while (cell.hasNext()) {
+							array.add(formattedCellValue(cell.next()));
+						}
+					}
+				}
+			}
+		}
+		return array;
+	}
+
+	private String formattedCellValue(Cell cell) {
+		if (cell.getCellType() == CellType.STRING) {
+			return cell.getStringCellValue();
+		} else {
+			return NumberToTextConverter.toText(cell.getNumericCellValue());
+		}
+	}
+
 }
--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class testSample {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TestSample {
+
+	public static void main(String[] adrgs) throws IOException {
+		DataDriven dataDriven = new DataDriven();
+
+		// get data in format Key => value for the whole row
+		HashMap<String, String> data1 = dataDriven.getFullData("/testcases.xlsx", "TestCases", "TC-03");
+		HashMap<String, String> data2 = dataDriven.getFullData("/testdata.xlsx", "TestData", "Purchase");
+
+		//get data in array format [rowName, data0, data1, data2] for the row (with row name)
+		ArrayList<String> data3 = dataDriven.getData("/testdata.xlsx", "TestData", "Add Profile");
+		ArrayList<String> data4 = dataDriven.getData("/testcases.xlsx", "TestCases", "TC-03");
+		System.out.println(data1);
+		System.out.println(data2);
+		System.out.println(data3);
+		System.out.println(data4);
+	}
 }
